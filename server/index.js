e7b8b8c30e7455c1bd13c5c796d779dcb7cc00f5
Ilya <44939563+zabozlaev@users.noreply.github.com>
2019-02-23 23:42:25 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,34 +2,36 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
-
 const bodyParser = require("body-parser");
 
 const mongoose = require("mongoose");
 
 const { URI, PORT } = require("./config/config.js");
 
-const setRoutes = require("./routes.js");
+const AppRoutes = require("./routes.js");
 
-app.use(require("morgan")("combined"));
+module.exports.start = async () => {
+  app.use(cors());
 
-app.use(bodyParser.json());
+  app.use(require("morgan")("combined"));
 
-setRoutes(app);
+  app.use(bodyParser.json());
 
-mongoose
-  .connect(
-    URI,
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true
-    }
-  )
-  .then(() => {
-    console.log(`[DATABASE] - CONNECTED`);
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`[SERVER] - LISTENING ON ${PORT}`);
-    });
-  })
-  .catch(e => console.log(e));
+  AppRoutes(app);
+
+  mongoose
+    .connect(
+      URI,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true
+      }
+    )
+    .then(() => {
+      console.log(`[DATABASE] - CONNECTED`);
+      app.listen(process.env.PORT || 8000, () => {
+        console.log(`[SERVER] - LISTENING ON ${PORT}`);
+      });
+    })
+    .catch(e => console.log(e));
+};
